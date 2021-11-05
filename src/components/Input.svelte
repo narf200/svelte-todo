d2c16e2be0ffa9f93e038ddee708ec974872d06f
narf200 <narf200@gmail.com>
2021-11-05 12:48:28 +0500

    first commit

---
+++ src/components/Input.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+
+    export let value = '';
+    export let placeholder = '';
+
+    const dispatch = createEventDispatcher();
+
+    const onInput = (event: any) => {
+        value = event.target.value;
+    }
+
+    const onKeydown = (event: KeyboardEvent) => {
+        if(event.code === 'Enter') {
+            dispatch('enter')
+        }
+    }
+
+</script>
+
+<input
+        type="text"
+        class="input"
+        {value}
+        {placeholder}
+        on:input={onInput}
+        on:keydown={onKeydown}
+>
+
+<style lang="scss">
+    .input {
+      padding: 10px 20px;
+      border: 1px solid rgba(#000, 0.1);
+      border-radius: 5px;
+      outline: none;
+    }
+</style>
